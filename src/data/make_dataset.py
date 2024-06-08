14ee126d45bcff22f70b5b47f7be637905b8d44f
Konrad Binko <59764236+kbinko@users.noreply.github.com>
2024-06-08 18:39:04 +0000

    .

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,17 +5,37 @@ from glob import glob
 # Read single CSV file
 # --------------------------------------------------------------
 
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
 
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
 
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
+
+files = glob("../../data/raw/MetaMotion/*.csv")
 
 
 # --------------------------------------------------------------
 # Extract features from filename
 # --------------------------------------------------------------
 
+data_path = "../../data/raw/MetaMotion/"
+f = files[0]
+
+participant = f.split("-")[0].replace(data_path, "")
+label = f.split("-")[1]
+category = f.split("-")[2].rstrip("123_MetaWear_2019")
+
+df = pd.read_csv(f)
+
+df["participant"] = participant
+df["label"] = label
+df["category"] = category
 
 # --------------------------------------------------------------
 # Read all files
